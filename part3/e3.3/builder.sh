52ac00385263a2b97b438562bc70adb14528b18e
frensing <dev@frensing.de>
2023-03-14 13:27:34 +0200

    e3.3

---
+++ part3/e3.3/builder.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# Cloning Repo
+git clone https://github.com/$1.git tmp_repo
+
+# Build and Push
+docker build tmp_repo -t $2
+docker push $2
+
+# Cleaning up
+rm -rf tmp_repo
+docker image rm $2
